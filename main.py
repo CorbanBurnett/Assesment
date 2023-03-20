3bc6bf6fad97946dc5903e5831a9d0430e900704
Corban <126821942+CorbanBurnett@users.noreply.github.com>
2023-03-20 01:47:22 +0000

    i added some intro things

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-Testing
+#this is going to be a program that quizzes year 9's about rangiora high school
+#Importing time
+import time
+print ("Welcome to the quiz for year 9's! ")
+time.sleep (0.5)
+name = input ("what is your name? ")
+time.sleep (0.5)
+print ("hello" ,name, "!")
