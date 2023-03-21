a46a01009736d9a7e3bc2850a64891a24c17641c
Corban <126821942+CorbanBurnett@users.noreply.github.com>
2023-03-21 23:03:59 +0000

    wrote the answers

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
-#this is going to be a program that quizzes year 9's about rangiora high school
 #Importing time
 import time
+
+#introducing the quizz
+
 print ("Welcome to the quiz for year 9's! ")
 time.sleep (0.5)
 name = input ("what is your name? ")
 time.sleep (0.5)
 print ("hello" ,name, "!")
+#create 2 list of questions and answers
+questions = [
+  "what is the appropriate color of shoes you should wear?",
+  "What day is the shortest school day?",
+  "what color is rutherford?",
+  "How many houses are in Rangiora High School?",
+  "Where do you usally do ako?",
+  "how many blocks in this school are 2 stories?",
+  "what classes are usally in s2?",
+  "How many terms are there in a school year?",
+  "What subject is usally in C-Block?",
+  "what classes are mainly in G-Block",
+
+]
+answers = [
+  "Black",
+  "Wednesday",
+  "Blue",
+  "6",
+  "Rakahuri",
+  "3",
+  "Health",
+  "4",
+  "Maths",
+  "English",
+]
+
